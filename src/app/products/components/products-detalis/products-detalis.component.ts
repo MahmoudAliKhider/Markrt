c527f47d537b50755a4cf486bfea586a1d25d39b
mahmoudAliKhider <mahmoudkhider37@gmail.com>
2022-08-01 18:15:51 +0200

    Add data to CartsPage

--- src/app/products/components/products-detalis/products-detalis.component.ts
+++ src/app/products/components/products-detalis/products-detalis.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-products-detalis',
@@ -8,11 +9,18 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductsDetalisComponent implements OnInit {
  id:any
-  constructor(private route:ActivatedRoute) { 
+ data:any={}
+  constructor(private route:ActivatedRoute,private service:ProductsService) { 
     this.id = this.route.snapshot.paramMap.get("id")
   }
 
   ngOnInit(): void {
+    this.getProducts()
   }
-
+  getProducts()
+  {
+   this.service.getProductsById(this.id).subscribe(res=>{
+    this.data=res
+   })
+  }
 }
